633dc525911e90f5483292b667b4bcb8e36053ae
nicole-russack <russack@usc.edu>
2022-09-25 13:19:49 -0700

    Updates

--- src/Pages/js/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from './About';
-export { default as Resume } from './Resume';
-export { default as Home } from './Home';
-export { default as Portfolio } from './Portfolio';
